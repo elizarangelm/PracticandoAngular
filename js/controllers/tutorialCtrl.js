bb6c3616310fc2c826eca0916b94620830da8e3a
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2022-01-11 07:54:12 -0500

    Directive

--- js/controllers/tutorialCtrl.js
+++ js/controllers/tutorialCtrl.js
@@ -3,7 +3,7 @@ angular.module('tutorialCtrlModule', [])
     .controller("TutorialCtrl", ["$scope", function ($scope) {
         $scope.tutorialObject = {};
         $scope.tutorialObject.name = "Elizabeth";
-        $scope.tutorialObject.lastname = "Rangel";        
+        $scope.tutorialObject.lastname = "Rangel";
         $scope.tutorialObject.title = "MainPage";
         $scope.tutorialObject.subtitle = "subtitle";
         $scope.tutorialObject.bindOutput = 2
@@ -11,5 +11,13 @@ angular.module('tutorialCtrlModule', [])
         $scope.timeTwo = function () {
             $scope.tutorialObject.bindOutput *= 2;
         }
-    }]);
+    }])
+    .directive("erWelcomeMessage", function () {
+        return {
+            //A: atributo, E: elemento, M: comentario, C: Class
+            restrict: "AE",
+            template: "<div>How u doin'?</div>"
+        }
+    })
+    ;
 
