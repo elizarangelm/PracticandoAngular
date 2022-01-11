c50547d91f86d4aca35f6b7938e3879127aae9ad
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2022-01-11 08:05:00 -0500

    orden

--- js/controllers/tutorialCtrl.js
+++ js/controllers/tutorialCtrl.js
@@ -1,11 +1,15 @@
 angular.module('tutorialCtrlModule', [])
     //scope es un servicio con $, lleva el controlador a la vista, se refiere al modelo,
     .controller("TutorialCtrl", ["$scope", "Calculations", function ($scope, Calculations) {
+
         $scope.tutorialObject = {};
+
         $scope.tutorialObject.name = "Elizabeth";
         $scope.tutorialObject.lastname = "Rangel";
+
         $scope.tutorialObject.title = "MainPage";
         $scope.tutorialObject.subtitle = "subtitle";
+
         $scope.tutorialObject.bindOutput = 2
 
         $scope.timeTwo = function () {
@@ -15,20 +19,25 @@ angular.module('tutorialCtrlModule', [])
         }
         Calculations.pitagorasTeorema()
     }])
+
     .directive("erWelcomeMessage", function () {
+
         return {
             //A: atributo, E: elemento, M: comentario, C: Class
             restrict: "AE",
             template: "<div>How u doin'?</div>"
         }
+
     })
 
     //crear servicio
     .factory("Calculations", function () {
+
         var calculations = {};
         calculations.timeTwo = function (a) {
             return a * 2;
         };
+
         calculations.pitagorasTeorema = function (a, b) {
             return (a * a) + (b * b);
         }
