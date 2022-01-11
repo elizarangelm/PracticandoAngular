f95c19413e020735e3fa9c65df32547a22df9b8f
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2022-01-11 07:36:47 -0500

    Data binding practice

--- js/controllers/tutorialCtrl.js
+++ js/controllers/tutorialCtrl.js
@@ -1,6 +1,15 @@
 angular.module('tutorialCtrlModule', [])
 
     .controller("TutorialCtrl", ["$scope", function ($scope) {
+        $scope.tutorialObject = {};
+        $scope.tutorialObject.name = "Elizabeth";
+        $scope.tutorialObject.lastname = "Rangel";        
+        $scope.tutorialObject.title = "MainPage";
+        $scope.tutorialObject.subtitle = "subtitle";
+        $scope.tutorialObject.bindOutput = 2
 
+        $scope.timeTwo = function () {
+            $scope.tutorialObject.bindOutput *= 2;
+        }
     }]);
 
